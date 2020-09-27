2d507ca36118e98d03073f304e761a5a29a2956c
DEtheFree <diegoe2017@gmail.com>
2020-09-27 14:19:57 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,5 +1,5 @@
 
-let hats = [{type: "tyrolean", color: "red"},{type: "stenton", color: evergreen},{type: "top hat". colro: "black"},{type: "bowler", color: "black"},{type: "fez", color: "blue"}];
+let hats = [{type: "tyrolean", color: "red"},{type: "stenton", color: "evergreen"},{type: "top hat". colro: "black"},{type: "bowler", color: "black"},{type: "fez", color: "blue"}];
 
 let randomIndex;
 

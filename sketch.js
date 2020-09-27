9363529bd36e878d3cbf0a2c978facd0e9fbd601
DEtheFree <diegoe2017@gmail.com>
2020-09-27 14:12:34 -0700

    text

--- sketch.js
+++ sketch.js
@@ -1,9 +1,15 @@
 
 let hats = [{type: "tyrolean", color: "red"},{type: "stenton", color: evergreen},{type: "top hat". colro: "black"},{type: "bowler", color: "black"},{type: "fez", color: "blue"}];
 
+let randomIndex;
+
 function setup() {
   createCanvas(400, 400);
   background(220);
+
+
+randomIndex = int(random(hats.length))
+text(hats[randomIndex].type)
 
 }
 

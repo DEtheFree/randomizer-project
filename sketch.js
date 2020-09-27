11f4e533eda58fc98b532743d80a3ed664076cc9
DEtheFree <diegoe2017@gmail.com>
2020-09-27 13:51:02 -0700

    update

--- sketch.js
+++ sketch.js
@@ -1,7 +1,10 @@
+
+let hats = [{type: "tyrolean", color: "red"},{type: "stenton", color: evergreen},{type: "top hat". colro: "black"},{type: "bowler", color: "black"},{type: "fez", color: "blue"}];
+
 function setup() {
   createCanvas(400, 400);
   background(220);
-  square(40,60,70)
+
 }
 
 
